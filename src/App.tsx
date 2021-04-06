eb2e8aff78446f2be9ce6cbba7b300ee16851267
Pavel Khlebko <pavel.khlebko@thomsonreuters.com>
2021-04-06 20:50:37 +0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Courses app</h1>
+        <div>Login</div>
+        <Welcome name="Hell"></Welcome>
       </header>
+      <section><CoursesList courses={mockCourses}></CoursesList></section>
+      <footer>(c) 2021 Courses App</footer>
     </div>
   );
 }
 
 export default App;
+
+function Welcome(props: any): JSX.Element {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function CoursesList(props: any): JSX.Element {
+  const courses: CourseModel[] = props.courses;
+  const cL = courses.map((course: any) =>
+    <li key={course.id.toString()}>
+      <CourseComponent course={course}></CourseComponent>
+    </li>);
+
+  return (
+    <ul>{cL}</ul>
+  );
+}
+
+function CourseComponent(props: any) {
+  const course: CourseModel = props.course;
+  const {date, title, description} = course;
+
+  return (<div>Test {title} {date.toDateString()} {description}</div>);
+}
+
+export interface CourseModel {
+  id: number;
+  title: string;
+  description: string;
+  authorID: string;
+  date: Date;
+  rating: number;
+}
+
+export const mockCourses: CourseModel[] = [
+  {
+    id: 1,
+    title: 'Angular 4',
+    description: 'Angular 4 course',
+    authorID: 'pablo',
+    date: new Date(),
+    rating: 3
+  },
+  {
+    id: 2,
+    title: 'Angular.js',
+    description: 'Angular.js course',
+    authorID: 'mike',
+    date: new Date(),
+    rating: 5
+  }
+];
